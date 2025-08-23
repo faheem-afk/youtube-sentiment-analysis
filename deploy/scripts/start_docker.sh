c8a56719dbcffe81b14b8ffe1b2ef3ad173bcea3
= <adahm7114@gmail.com>
2025-08-23 18:18:31 +0100

    deployment finalized

--- deploy/scripts/start_docker.sh
+++ deploy/scripts/start_docker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Log everything to start_docker.log
-exec > /home/ubuntu/start_docker.log 2>&1
-
 echo "Logging in to ECR..."
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 596514762357.dkr.ecr.us-east-1.amazonaws.com
 
@@ -19,4 +16,3 @@ fi
 
 echo "Starting new container..."
 docker run -d -p 80:5000 --name container-app 596514762357.dkr.ecr.us-east-1.amazonaws.com/ysa-ecr:latest
-
