99580cfa12cddc4f5d85539e6f0f24ed661a0db4
= <adahm7114@gmail.com>
2025-08-23 14:23:34 +0100

    deployment finalized

---
+++ deploy/scripts/start_docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Log everything to start_docker.log
+exec > /home/ubuntu/start_docker.log 2>&1
+
+echo "Logging in to ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 596514762357.dkr.ecr.us-east-1.amazonaws.com
+
+echo "Pulling Docker Image"
+docker pull 596514762357.dkr.ecr.us-east-1.amazonaws.com/ysa-ecr:latest
+
+echo "Checking for existing container"
+if [ "$(docker ps -q -f name=container-app)" ]; then
+    echo "Stopping existing container..."
+    docker stop container-app
+    echo "Removing existing container..."
+    docker rm container-app
+if
+
+echo "Starting new container..."
+docker run -d -p 80:5000 --name container-app 596514762357.dkr.ecr.us-east-1.amazonaws.com/ysa-ecr:latest
+
